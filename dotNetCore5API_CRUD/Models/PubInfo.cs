097e8e63811549e727845fd9ff2774e07bf0b459
walter <walterlin0102@bestcom.com.tw>
2021-05-25 17:07:56 +0800

    init
    dot net core API practice
    init code,
    add EF models(db first)

---
+++ dotNetCore5API_CRUD/Models/PubInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace dotNetCore5API_CRUD.Models
+{
+    public partial class PubInfo
+    {
+        public string PubId { get; set; }
+        public byte[] Logo { get; set; }
+        public string PrInfo { get; set; }
+
+        public virtual Publisher Pub { get; set; }
+    }
+}
